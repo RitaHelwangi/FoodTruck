30015defb317bd1867998d92f6ffcb5e68a39101
Rita <rita.helwangi@gmail.com>
2024-12-13 17:00:39 +0100

    creat menu id /btn/hide functions

--- app.js
+++ app.js
@@ -17,16 +17,15 @@ const options = {
   method: "GET",
   headers: {
     "Content-Type": "application/json",
+	accept: "application/json",
     "x-zocom": key,
   },
 };
 
+let cart = {};
+let getOrder = "";
+
 let url = "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com/menu";
-
-fetchMenuItems()
-
-async function fetchMenuItems() {
-  try {
 
 const wontonResponse = await fetch(url + "?type=wonton", options);
 const wontonData = await wontonResponse.json();
@@ -45,8 +44,13 @@ console.log(drinkData.items);*/
 
 const dipsBtn = document.querySelector(".dips-btn");
 const drinksBtn = document.querySelector(".drinks-btn");
-const cartBtn = document.querySelector(".cart-btn");
 const menuVy = document.querySelector(".menu-vy");
+const cartBtn = document.querySelector(".cart-btn");
+const etaVy = document.querySelector(".eta-vy");
+const receiptVy = document.querySelector(".receipt-vy");
+const orderList = document.querySelector(".order-list");
+const numberOfItemInCart = document.querySelector(".note-cart");
+const SendOrder = document.querySelector("send-order");
 
 
 
@@ -63,48 +67,126 @@ drinkData.items.forEach((item) => {
   createDipOrDrinkList(item, drinksBtn);
 });
 
-cartBtn.addEventListener("click", () => {
-  hideAllPages();
-  menuVy.classList.remove("hide");
+document.querySelector(".cart-btn").addEventListener("click", ()=> {
+	hideAllPages();
+	cartVy.classList.remove("hide");
+
+	for (const [key, value] of Object.entries(cart)) {
+		renderOrderCart(key, value);
+		updateTotalPrice();
+	}
 });
-  } catch (error) {
-    console.error("Error fetching menu data:", error);
-  }
+
+document.querySelector(".home").addEventListener("click", ()=> {
+	hideAllPages();
+	menuVy.classList.remove("hide");
+	orderList.innerHTML = "";
+});
+
+document.querySelector("#order").addEventListener("click", () => {
+if (Object.keys(cart).length ===0) {
+	return
 }
+else {
+	hideAllPages();
+	etaVy.classList.remove("hide");
+	SendOrder();
+}
+});
+
+document.querySelector(".menu").addEventListener("click", (event) => {
+	if (event.target.closest(".item")) {
+		const item = event.target.closest(".item");
+		updateCart(item.getAttribute("orderId"));
+	}
+	updateCartNumber();
+});
+
+document.querySelector(".dips.btn").addEventListener("click", (event) =>{
+	if (event.currentTarget.closest("button")) {
+		const item = event.target.closest("button");
+		updateCart(item.getAttribute("orderId"));
+	}
+	updateCartNumber();
+});
+
+document.querySelector(".drinks.btn").addEventListener("click", (event) =>{
+	if (event.currentTarget.closest("button")) {
+		const item = event.target.closest("button");
+		
+		updateCart(item.getAttribute("orderId"));
+		updateCartNumber();
+	}
+});
+
+document.querySelector(".fade-btn")("click" , () => {
+	hideAllPages();
+	receiptVy.classList.remove("hide");
+	renderOutReceipt();
+});
+
+document.querySelectorAll(".new-order").forEach((button) =>{
+	button.addEventListener("click", () => {
+		hideAllPages();
+		menuVy.classList.remove("hide");
+		orderResetHandler();
+	});
+});
+
+function orderResetHandler() {
+	const item = document.querySelectorAll("[orderId]");
+	cart = {};
+	orderList.innerHTML = "";
+	SendOrder.innerHTML =""
+	updateCartNumber();
+	updateTotalPrice();
+
+	const orderId =document.querySelectorAll(".order-id");
+	orderId.forEach((order) => {
+		order.innerText = "";
+	});
+}
+
+
 // menu items
 function createMenuItem(food) {
   const menu = document.querySelector(".menu");
 
-    if (!menu) {
-    console.error("Menu container not found.");
-    return;
-  }
-
-
   let div = document.createElement("div");
   div.classList.add("item");
-  
-  let h2 = document.createElement("h2");
-  h2.innerText = food.name;
+  div.setAttribute("orderId", food.id);
 
   let span = document.createElement("span");
   span.classList.add("dots");
+  let h2 = document.createElement("h2");
+  h2.innerText = food.name;
+  h2.append(span);
+  h2.append(`${food.price} SEK`);
+  let p = document.createElement("p");
 
-  div.appendChild(h2);
-  div.appendChild(span);
-  menu.appendChild(div);
+  food.ingredients.forEach((type) => {
+    p.innerText += `${type}, `;
+  });
+
+  p.innerText = p.innerText.slice(0, -2);
+
+  div.append(h2, p);
+  menu.append(div);
 }
 
+//dips n drinks
+function createDipOrDrinkList(type, sort) {
+ let button = document.createElement("button");
+ button.setAttribute("orderId", type.id);
+ button.innerText = type.name;
 
-function createDipOrDrinkList(item, parentElement) {
-  if (!parentElement) {
-    console.error("Parent element not found.");
-    return;
-  }
-
-  let div = document.createElement("div");
-  div.classList.add("item");
-  div.innerText = item.name;
-
-  parentElement.appendChild(div);
+ sort.append(button);
 }
+
+ //hide vy
+ function hideAllPages() {
+	cartBtn.classList.add("hide");
+	etaVy.classList.add("hide");
+	receiptVy.classList.add("hide");
+	menuVy.classList.add("hide");
+ }
